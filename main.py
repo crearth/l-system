e19914e68e6e972af6c1f254535a4e356c3f6b1b
matias <matiasdaneels@gmail.com>
2021-10-27 19:20:52 +0200

    Add drawing function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Import json library
+# Import libraries
 import json
+import turtle as tur
 
 # Getting input on what json file to use
 def getFile():
@@ -38,6 +39,26 @@ def lSystem(axiom, rules, iter):
 
 	return old_string
 
+# Draw the given string by using the translations
+def draw(string, trans):
+	screen = tur.getscreen() # Make new screen where the drawing will cmom
+	t = tur.Turtle() # Initialize the turtle and give it the name "t"
+
+	for el in string:
+		if el in trans: # Check if the el can get translated
+			para = trans[el][1] # Para is the parameter that will be put in the used fuction
+			if "draw" == trans[el][0]:
+				t.forward(para) # Draw line with length para
+			elif "angle" == trans[el][0]:
+				t.left(para) # Rotate to the left with "para" degrees
+			elif "forward" == trans[el][0]:
+				# Move forward without drawing a line
+				t.penup() # Raising pen
+				t.forward(para) # Moving
+				t.pendown() # Dropping pen, draw again
+			elif "nop" == trans[el][0]:
+				pass
+
 # Main function
 def main():
 
@@ -49,6 +70,7 @@ def main():
 	iter = int(input("How many iterations of the given l-system do you want?: "))
 
 	lstring = lSystem(axiom, rules, iter)
-	print(lstring)
+	draw(lstring, trans)
+	tur.Screen().exitonclick() # Keep the drawing open unless you click on exit button
 
 main()
