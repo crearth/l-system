70a09fcfa4bde7f917ab1111b36bfac1a89823a2
Arthur <arthur.cremelie@gmail.com>
2021-11-03 15:36:19 +0100

    add comments

--- unitTesting.py
+++ unitTesting.py
@@ -1,10 +1,13 @@
 from main import getData, getVariables, lSystem
 import pytest
 
-data = getData("./tests/test1.json")
+f = open("./tests/test1.json")
+data = getData(f)
 axiom, rules, variables, constants, trans = getVariables(data)
 
+# First test
 def test_lSystem(axiom, rules):
 	iter = 4 
 	lstring = lSystem(axiom, rules, iter)
 	assert lstring == "A+B+A-B+A+B-A-B+A+B+A-B-A+B-A-B"
+
