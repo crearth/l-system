42073b83065cc4aa93b641df5a4c9181423332ca
Arthur <arthur.cremelie@gmail.com>
2021-11-25 11:38:38 +0100

    update backup shell create directory

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 
 DATE=$(date +"%Y-%m-%d-%H:%M:%S") # Make variable DATE with the current date
 cp -a history.txt "history-$DATE" # Copy the current history.txt file with new name with the DATE varible
+mkdir ~/.l-systems/ # Create .l-systems directory in the home directory
 mv "history-$DATE" ~/.l-systems/ # Move the new file to the .l-systems in the home directory
